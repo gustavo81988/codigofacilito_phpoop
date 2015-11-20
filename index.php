6eadaac5736713e911ffe2cc8fc8166445f4de1b
Gustavo <gustavo81988@gmail.com>
2015-11-20 09:29:17 -0430

    Clases y Objetos

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+class Persona{
+    
+    //Atributos
+    public $nombre = "Pedro";
+    
+    //Metodos
+    
+    public function hablar($mensaje){
+        echo $mensaje;
+    }
+    
+}
+
+$persona = new Persona();
+echo $persona->nombre." ";
+echo $persona->hablar("Saludos de codigo facilito");
+
