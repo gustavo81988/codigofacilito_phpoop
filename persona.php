bbf7585f75952342171c8eb17c0dda9c3f10a642
Gustavo Ramirez <gustavo.ramirez@ingeniandoweb.com>
2015-12-03 14:20:28 -0430

    3-Metodos y atributos

--- persona.php
+++ persona.php
@@ -3,17 +3,37 @@
 class Persona{
     
     //Atributos
-    public $nombre = "Pedro";
+    public $nombre = array();
+    public $apellido = array();
     
     //Metodos
     
+    public function guardar($nombre,$apellido){
+        $this->nombre[] = $nombre;
+        $this->apellido[] = $apellido;
+    }
+    
+    public function mostrar(){
+        for($i=0; $i < count($this->nombre); $i++){
+            self::formato($this->nombre[$i],$this->apellido[$i]);
+        }
+    }    
+    
+    public function formato($nombre,$apellido){
+        echo "-Nombre: ".$nombre. "<br>-Apellido: ".$apellido."<br><br>";
+    } 
+
+
     public function hablar($mensaje){
         echo $mensaje;
     }
     
 }
 
-//$persona = new Persona();
+$persona = new Persona();
+$persona->guardar("Gustavo", "Ramirez");
+$persona->guardar("Mariangel", "Bermudez");
+$persona->mostrar();
 //echo $persona->nombre." ";
 //echo $persona->hablar("Saludos de codigo facilito");
 
